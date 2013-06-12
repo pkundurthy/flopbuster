8e6c405e090120c055060ade7b321fef82bfaffc
Praveen Kundurthy <p.kundurthy7@gmail.com>
2013-06-12 01:17:34 -0700

    adding some improved data files

--- scrapers.py
+++ scrapers.py
@@ -104,14 +104,17 @@ class ImdbAPIFunction:
 
     BASE_URL = 'http://www.omdbapi.com'
 
-    def __init__(self, title):
+    def __init__(self, title,release):
         self.title = title
+        self.release = release
         self._process()
         
     def _process(self):
         movie = re.sub(' ','+',self.title)
+        yySplit = map(self.year.split('/'))
+        yySplit[-1]
 
-        url = "%s/?i=&t=%s" % (self.BASE_URL, movie)
+        url = "%s/?i=&t=%s&y=%s" % (self.BASE_URL, movie,year)
         content = urllib2.urlopen(url).read()
         content = json.loads(content)
         self.data = content
